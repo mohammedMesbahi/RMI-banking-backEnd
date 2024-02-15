488b1a0f9f32bd0bdc1f9a4eddf49db057ef7560
Haitam Belcaida <haitambelcaida@gmail.com>
2024-02-15 19:47:04 +0100

    update

--- src/main/java/estm/dsic/umi/Main.java
+++ src/main/java/estm/dsic/umi/Main.java
@@ -7,42 +7,53 @@ import estm.dsic.umi.dao.UserDaoJDBC;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        User user1 = new User();
+        /*User user1 = new User();
         user1.setEmail("yassin.ed@gmail.com");
         user1.setPassword("123456");
         UserDaoJDBC.getInstance().create(user1);
-        System.out.println(UserDaoJDBC.getInstance().getById(user1.getId()));*/
+        System.out.println(UserDaoJDBC.getInstance().getById(user1.getId()));
+        */
+
+        /*User user2 = new User();
+        user2.setEmail("mesbahi.ed@gmail.com");
+        user2.setPassword("123456");
+        UserDaoJDBC.getInstance().create(user2);
+        System.out.println(UserDaoJDBC.getInstance().getById(user2.getId()));*/
+
 
         /* create an account */
         /*Account account1 = new Account();
         account1.setBalance(1000.0);
-        account1.setOwnerId(6);
+        account1.setOwnerId(1);
 
         System.out.println(DefaultAccountService.getInstance().createAccount(account1));*/
+
+        /*Account account2 = new Account();
+        account2.setBalance(9000.0);
+        account2.setOwnerId(2);
+
+        System.out.println(DefaultAccountService.getInstance().createAccount(account2));*/
 
         /* fetch user accounts + transactions of each account*/
         /*User userMesbahi = new User();
         userMesbahi.setEmail("mesbahi.ed@gmail.com");
         userMesbahi.setPassword("123456");
-        userMesbahi.setId(1);
+        userMesbahi.setId(2);
 
         System.out.println(DefaultAccountService.getInstance().getUserAccounts(userMesbahi));*/
 
         /*MAKE A DEPOSIT*/
         User userMesbahi = new User();
-        userMesbahi.setEmail("mesbahi.ed@gmail.com");
-        userMesbahi.setPassword("123456");
-        userMesbahi.setId(1);
+
+        userMesbahi.setId(2);
 
         userMesbahi.setAccounts(
                 DefaultAccountService.getInstance().getUserAccounts(userMesbahi)
         );
 
         Account account = userMesbahi.getAccounts().get(0);
-
         // deposit 7000.0
-        // System.out.println(DefaultAccountService.getInstance().deposit(account, 7000.0));
+        /*System.out.println(DefaultAccountService.getInstance().deposit(account, 1000.0));*/
 
         // withdraw 5000.0
         System.out.println(DefaultAccountService.getInstance().withdraw(account, 500.0));
